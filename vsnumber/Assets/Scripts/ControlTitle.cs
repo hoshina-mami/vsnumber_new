deba132bb3bf83b8c4f7801ed7aca356eb576443
hoshina_mami <mamimomo722@gmail.com>
2016-05-07 15:19:06 +0900

    uTween追加

--- vsnumber/Assets/Scripts/ControlTitle.cs
+++ vsnumber/Assets/Scripts/ControlTitle.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ControlTitle : MonoBehaviour {
 
+	//private
+	private GameObject Btn_vs;
+	private GameObject Btn_single;
+
 	// Use this for initialization
 	void Start () {
         Application.targetFrameRate = 60;
+
+        Btn_vs          = GameObject.Find("Btn_vs");
+        Btn_single      = GameObject.Find("Btn_single");
 	
 	}
 	
@@ -14,8 +22,28 @@ public class ControlTitle : MonoBehaviour {
 	
 	}
 
-    // VS Play選択画面へとぶ
-    public void LoadMainVS () {
-        Application.LoadLevel("MainVS");
+	// SingleModeボタンを選択
+	public void tapSigleMode () {
+
+		//GetComponent<AudioSource>().Play();
+
+		hideObjects ();
+
+		Invoke("LoadInGameSingle",  0.9f);
+
+	}
+
+    // SingleMode選択画面へとぶ
+    public void LoadInGameSingle () {
+        //Application.LoadLevel("InGameSingle");
     }
+
+
+    //画面遷移のさいにオブジェクトを消す
+	void hideObjects () {
+
+		//makes object's alpha 0
+		Btn_vs.GetComponent<uTools.uTweenAlpha> ().enabled = true;
+		
+	}
 }
