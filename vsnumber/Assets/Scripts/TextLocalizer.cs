2aa014a371d59a3abd4c356b73e7c4113803035a
hoshina_mami <mamimomo722@gmail.com>
2016-09-22 15:16:31 +0900

    バージョンとか書いた

---
+++ vsnumber/Assets/Scripts/TextLocalizer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextLocalizer : MonoBehaviour {
+    [Multiline]
+    public string englishText;
+    public int englishFontSize;
+    void Start () {
+        Text text = GetComponent<Text> ();
+        if (Application.systemLanguage != SystemLanguage.Japanese) {
+            text.text = englishText;
+            if (englishFontSize != 0) {
+                text.fontSize = englishFontSize;
+            }
+        }
+    }
+}
